140918214c38b13b5fd7d30325452a7a71449bef
hrt34 <1220104346@qq.com>
2022-11-25 09:58:25 +0800

    speech_modify

--- lib/speechRecognize.dart
+++ lib/speechRecognize.dart
@@ -12,8 +12,8 @@ class SpeechRecognize {
     var Timestamp = dateTime.millisecondsSinceEpoch.toString().substring(0, 10);
     var Algorithm = "TC3-HMAC-SHA256";
     var CredentialScope = "$date/asr/tc3_request";
-    String SecretKey = "Ulqma9C5mGWhkkfOtBYuBKGxPi5Tipmj";
-    String SecretId = "AKIDxkIbjfOvG4f17zhCRSFmDMPK3Zu1uZ9i";
+    String SecretKey = " ";
+    String SecretId = " ";
 
     String HTTPRequestMethod = "POST";
     String CanonicalURI = "/";
